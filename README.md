f570fa3a99ca2a2eaef6414257228a96c90e4cfc
BenDz <bdiolez@gmail.com>
2019-02-19 15:33:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# AT Internet AdBack plugin
+
+## Introduction
+
+This plugin allows you to send AT Internet hits blocked on end-user browsers, thankd to Adback technology.
+
+## Pre-requisite
+
+* An account on [Adback.co](https://landing.adback.co/)
+* The usage of first-party user id in your smarttag.js
+* The activation of Callbacks in your smarttag.js file (default to true)
+
+## Usage
+
+Simply put this code anywhere on your website, where Adback and AT Internet are both running.
---
+++ adback.js
@@ -0,0 +1,16 @@
+window.ATInternet = window.ATInternet || {};
+window.ATInternet.Callbacks = window.ATInternet.Callbacks || {};
+window.ATInternet.Callbacks.Adback = window.ATInternet.Callbacks.Adback || function(tag) {
+  tag.onTrigger("Tracker:Hit:Sent:Error", (trigger, data) => {
+    var hitAt = data.details.hit;
+    var configSecure = tag.getConfig("secure");
+    var protocolSecure = document.location.protocol === "https:";
+    var isSecure = configSecure || protocolSecure;
+    var protocol = "";
+    if (hitAt.indexOf("http") != 0) { protocol = isSecure ? "https:" : "http:"; }
+    hitAt = protocol + hitAt;
+    window.parent[window.parent.document.getElementById("adback_data").getAttribute("data-adback")].API().sendXitiPixel(hitAt);
+  });
+};
+window.ATInternet.Utils = window.ATInternet.Utils || {dispatchCallbackEvent: function() {}};
+window.ATInternet.Utils.dispatchCallbackEvent("Adback");
